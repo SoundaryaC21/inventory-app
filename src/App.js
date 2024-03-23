aa61b21f8a479f831ef2f292c204120814f93aa6
Soundarya C <Soundaryac970@gmail.com>
2024-03-23 18:46:01 +0530

    inventory-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Inventory from "./components/inventory";
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Inventory />
+    </div>
+  );
+}
+
+export default App;
